4c25a42987a23250429043b180df3765a1d672cf
flffkd12 <flffkd12@gmail.com>
2025-07-01 15:11:02 +0900

    feat: 서버로부터 데이터 연동후 표시시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased
-        min-h-screen border-2 border-blue-500 flex flex-col`} 
+        min-h-screen flex flex-col`} 
       >
         <header>
           <nav className="flex">
@@ -35,8 +35,8 @@ export default function RootLayout({
           <Link href="/posts" className="p-2 rounded hover:bg-gray-100">글 목록</Link>
           </nav>
         </header>
-        <main className="border-2 border-green-500 flex-1">{children}</main>
-        <footer className="border-2 border-red-500">푸터</footer>
+        <main className="flex-1 flex flex-col">{children}</main>
+        <footer className="text-center p-2">푸터</footer>
       </body>
     </html>
   );
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Page() {
+
+const [posts, setPosts] = useState<{ id: number, title: string }[]>([]);
+
+useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+    .then(response => response.json())
+    .then(setPosts);
+}, []);
+
   return (
     <>
         <h1>글 목록</h1>
         
+        {posts.length === 0 && <div>로딩중...</div>}
+
         <ul>
-            <li>명언 1</li>
-            <li>명언 1</li>
+            {posts.map((post) => (
+                <li key={post.id}>{post.title}</li>
+            ))}
         </ul>
     </>
   );
